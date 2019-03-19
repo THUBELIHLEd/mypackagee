acf6642c56b10a2fa201429c1505a41176087fcd
Lucky Ndimande <Lucky.N@explore-datascience.net>
2019-03-19 17:14:49 +0200

    V3

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='mypackage',
+    name='mypackagee',
     version='0.1',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='EDSA example python package',
     long_description=open('README.md').read(),
     install_requires=['numpy'],
-    url='https://github.com/<username>/<package-name>',
-    author='<Your Name>',
-    author_email='<Your Email>'
+    url='https://https://github.com/THUBELIHLEd/mypackagee.git',
+    author='lucky ndimande',
+    author_email='thube7langa@gmail.com'
 )
